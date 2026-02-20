2502d6473fdc05b92e65adfa5f5ec93b75d51ad3
Cristiancuervoo <132379249+Cristiancuervoo@users.noreply.github.com>
2026-02-20 09:56:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE = "trinkgeld-pwa-v1";
+const CACHE = "trinkgeld-pwa-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener("fetch", (e) => {
